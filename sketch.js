955845e440e1120262a91b4b69f0c3a068429ba0
rashmi-sathya <72431927+rashmi-sathya@users.noreply.github.com>
2021-04-22 14:34:10 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+var marks=[30,35,45,38,40];
+function score()
+{
+  var sum=marks[0]+marks[1]+marks[2]+marks[3]+marks[4];
+  console.log(sum);
+}
+
+function setup() {
+  
+score();}
+
+function draw() {
+  background(220);
+}
